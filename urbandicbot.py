070719a9cbd6f6ef8f3244c6aef007a04a3580bc
iw4p <nimakbarzade@yahoo.com>
2019-12-12 11:25:21 +0330

    fix minor bugs

--- urbandicbot.py
+++ urbandicbot.py
@@ -7,17 +7,15 @@ from uuid import uuid4
 import urllib.request
 import requests
 import telegram
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 botToken = "TOKEN"
 
 bot = telegram.Bot(token=botToken)
 
-import logging
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
 userWord = ""
-userDefinition = ""
+definitionResult = ""
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 					level=logging.INFO)
@@ -26,16 +24,15 @@ logger = logging.getLogger(__name__)
 
 
 def start(update, context):
-	update.message.reply_text("Hi, for getting a word's definition in Urban dictionary just send me the word or use the inline group mode by adding it to the group, for more info press /help")
-
+	update.message.reply_text("Hi, for getting a word's definition in Urban dictionary just send me the word or use the inline mode for groups and private chats, for more info press /help")
 
 def help(update, context):
-	update.message.reply_text("Urban Dictionary is a crowdsourced online dictionary for slang words and phrases, operating under the motto 'Define Your World'. For searching a word's definition in Urban dictionary simply send the word or use it in your groups by typing '@ID + the word you're looking for' when it's added to the group. Hope you enjoy it.")
+	update.message.reply_text("Urban Dictionary is a crowdsourced online dictionary for slang words and phrases, operating under the motto 'Define Your World'. For searching a word's definition in Urban dictionary simply send the word or use it in your groups and private chats with others by typing '@ID + the word you're looking for'  without even adding it to the group. Hope you enjoy it.")
 
 #A scrapping code for getting the "result" meta code on UrbanDictionaty.com and copying it				
 def get_definition(word):
 	global userWord
-	global userDefinition
+	global definitionResult
 	
 	userWord = word
 
@@ -46,34 +43,34 @@ def get_definition(word):
 	if "og:description" in str(soup):
 		for tag in soup.find_all("meta"):
 			if tag.get("property", None) == "og: description":
-				userDefinition = tag.get("content", None)
+				definitionResult = tag.get("content", None)
 				return (tag.get("content", None))
 			if tag.get("property", None) == "og:description":
-				userDefinition = tag.get("content", None)
+				definitionResult = tag.get("content", None)
 				return (tag.get("content", None))
 	elif "Sorry, we couldn't find" in str(soup):
-		userDefinition = "Sorry, we couldn't find: " + word
+		definitionResult = "Sorry, we couldn't find: " + word
 		return ("Sorry, we couldn't find: " + word)
 
-#Reply the messages in private conversation
+#Reply the messages in private conversation with the bot
 def definition(update, context):
 	word = update.message.text
 	definition = get_definition(word)
 	update.message.reply_text(definition)
 	
-#Getting the definition of a word in groups in inline mode, while the bot is added to the group	
+#Getting the definition of a word in groups and private chats in inline mode, without adding the bot to the group
 def inlinequery(update, context):
 	global userWord
-	global userDefinition
+	global definitionResult
 	query = update.inline_query.query
 	get_definition(query)
 	results =[ 
 		InlineQueryResultArticle(
 			id=uuid4(),
 			title=userWord,
-			description=userDefinition,
+			description=definitionResult,
 			input_message_content=InputTextMessageContent(
-				message_text=userDefinition))
+				message_text=userWord + ": \n" + definitionResult))
 			]
 	
 	update.inline_query.answer(results)
